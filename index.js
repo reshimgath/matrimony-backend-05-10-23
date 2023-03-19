453e74e7b21d7d9b79b4fa670baf34cab7c45ae8
reshimgath <121487990+reshimgath@users.noreply.github.com>
2023-03-19 13:07:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ app.use(cors({
     allowedHeaders: '*'
 }));
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 //import routers
 const authUser = require("./api/User/authUser")
