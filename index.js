b77605168bdbacd8de6a4215222bae759827bef4
reshimgath <121487990+reshimgath@users.noreply.github.com>
2023-03-19 13:17:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3031 || process.env.port
 const connection = `mongodb+srv://reshimgath:${process.env.mongopassword}@cluster0.8qothpm.mongodb.net/reshimgath?retryWrites=true&w=majority`
 
 //const connection = `mongodb+srv://Muchmark:${process.env.mongopassword}@cluster0.irij3nk.mongodb.net/reshimgath?retryWrites=true&w=majority`
-// app.use(cors())
+app.use(cors())
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(express.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
